f0dc5109fb62c88c50916cf544ae8db7acee86d7
Edward Vozniak <voznyak.ed@gmail.com>
2024-11-03 20:03:43 +0200

    first master commit

---
+++ Lab5_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Lab5_1.cs
+// <Возняк Едвард>
+// Лабораторна робота № 5.1
+// Функції, що містять арифметичний вираз
+// Варіант 5
+class Lab5_1
+{   
+    static void Main(string[] args)
+    {
+        double s, t;
+
+        Console.WriteLine("Enter s value:");
+        s = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter t value:");
+        t = double.Parse(Console.ReadLine());
+
+        double c = (Math.Pow(h(s,t),4)+h(1.0,Math.Pow(s,2)+Math.Pow(t,2)))/(1.0+Math.Pow(h(s*t,1),2));
+
+        Console.WriteLine($"c = {c:F8}");
+    }
+    static double h( double a, double b)
+    {
+        return (a)/(Math.Pow(b,2)+1)+(1.0)/(Math.Pow(a,2)+Math.Pow(b,2));
+    }
+}
